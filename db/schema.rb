5cdcaf82e22cbe5f697223ff251275aae15572a5
Marc-Antoine Blais <blaisma@live.fr>
2022-05-17 11:49:22 -0400

    added default value to bookings status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_201553) do
+ActiveRecord::Schema.define(version: 2022_05_17_153841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_201553) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "life_id", null: false
     t.bigint "user_id", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.date "start_date"
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
