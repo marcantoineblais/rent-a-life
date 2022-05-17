16641a404c752d1888ea783f232be3ad694ef09e
florent gilet <florent.gilet@protonmail.com>
2022-05-17 11:40:07 -0400

    Created #Show in Bookings Controller and View

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,9 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.where(user: current_user)
   end
+
+  def show
+    @booking = params[:id]
+    @f = @booking.life
+  end
 end
