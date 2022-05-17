3de52c0419ac23dc360006732f9c8079843766d3
florent gilet <florent.gilet@protonmail.com>
2022-05-17 13:38:50 -0400

    #destroy was added to Lives Controller

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -2,4 +2,11 @@ class LivesController < ApplicationController
   def index
     @lives = Life.all
   end
+
+  def destroy
+    @life = Life.find(params[:id])
+    @life.destroy
+
+    redirect_to lives_path
+  end
 end
