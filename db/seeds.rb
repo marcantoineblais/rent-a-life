5785609a9922cfde42e6a0ecbf57b870c7a1e4eb
Marc-Antoine Blais <blaisma@live.fr>
2022-05-17 11:20:57 -0400

    user is now a variable (corrected 'false' when saving)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-Booking.delete_all
-Life.delete_all
-User.delete_all
+Booking.destroy_all
+Life.destroy_all
+User.destroy_all
 
-User.create(first_name: 'admin', last_name: "istrator", password: '123456', email: "a@a.ca")
+user = User.create(first_name: 'admin', last_name: "istrator", password: '123456', email: "a@a.ca")
 
 10.times do
-  Life.create(title: Faker::Artist.name, description: 'Fake life for tests', user_id: 1, price: 99.99)
+  Life.create(title: Faker::Artist.name, description: 'Fake life for tests', user: user, price: 99.99)
 end
 
 date = Date.today
 Life.all.each do |life|
   5.times do
-    Booking.create(life: life, user_id: 1, start_date: date, end_date: date.next_day)
+    Booking.create(life: life, user: user, start_date: date, end_date: date.next_day)
     date = date.next_day(2)
-    end
+  end
 end
