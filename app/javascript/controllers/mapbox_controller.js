c5d2cf3d566a068bdec6f4c7bb5978e0111595d6
Marc-Antoine Blais <blaisma@live.fr>
2022-05-19 19:37:06 -0400

    after merge

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -33,6 +33,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 150, maxZoom: 13, duration: 1200 })
   }
 }
